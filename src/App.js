f92ebdb4878ed6907a72f85ae538650ad616ba8e
Dominic Critchlow <critchlowd49@gmail.com>
2020-08-09 21:29:57 -0400

    First page header up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './Asset 4.svg';
 import './App.css';
 
 function App() {
@@ -7,18 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <hr></hr>
     </div>
   );
 }
